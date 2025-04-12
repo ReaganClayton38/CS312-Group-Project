5fb7b44eb6a0aa42256f3a645f213bfe6b71da85
Reagan Clayton <c837185728@helena.cs.colostate.edu>
2025-04-11 19:56:07 -0600

    Created form and validation

--- src/app/components/color-generator/color-generator.component.ts
+++ src/app/components/color-generator/color-generator.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-color-generator',
-  standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './color-generator.component.html',
   styleUrl: './color-generator.component.css'
 })
 export class ColorGeneratorComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      rows: [null, [Validators.required, Validators.min(1), Validators.max(1000)]],
+      columns: [null, [Validators.required, Validators.min(1), Validators.max(702)]],
+      colors: [null, [Validators.required, Validators.min(1), Validators.max(10)]]
+    });
+  }
+
+  get rowsValue() {
+    return this.form.get("rows");
+  }
+
+  get columnsValue() {
+    return this.form.get("columns");
+  }
+
+  get colorsValue() {
+    return this.form.get("colors");
+  }
 }
